91e9b3ceedf9653fd406ecac256439b70f08d473
yarik21102 <yaroslavmamchur5@gmail.com>
2021-10-17 01:10:53 +0300

    Лаба 3

--- lab03/src/main.c
+++ lab03/src/main.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-int n = 123;
-int d1 = n % 10;
-int d2 = n / 10 % 10;
-int d3 = n / 100 % 10;
-int res = d1 * 100 + d2 * 10 + d3;
-return 0;
+	int n = 123;
+	int d1 = n % 10;
+	int d2 = n / 10 % 10;
+	int d3 = n / 100 % 10;
+	int res = d1 * 100 + d2 * 10 + d3;
+	return 0;
 }
