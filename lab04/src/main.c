f359905a8517e3dbcddfcd1038cfa4e06c983b79
yarik21102 <yaroslavmamchur5@gmail.com>
2021-10-17 01:33:24 +0300

    Лаба 4

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -1,14 +1,10 @@
 //задание номер 20
-#include <stdio.h>
 int main()
 {
-double r = 5;
-double PI = 3.14;
-float l = 2*PI*r;
-float s = PI*(r*r);
- float v =4/3*PI*(r*r*r);
-printf(l);
-printf(s);
-printf(v);
+	double r = 5;
+	double PI = 3.14;
+	float l = 2 * PI * r;
+	float s = PI * (r * r);
+	float v = 4 / 3 * PI * (r * r * r);
+	return 0;
 }
-
