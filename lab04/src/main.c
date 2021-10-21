e0c7fe67cd2963c969be7cfc25bdeb74ccb3ea82
Vlad Tsupka <vlad19032004li@gmail.com>
2021-10-21 11:46:56 +0300

    лаба 4

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -1,10 +1,8 @@
 //задание номер 20
 int main()
 {
-	double r = 5;
-	double PI = 3.14;
-	float l = 2 * PI * r;
-	float s = PI * (r * r);
-	float v = 4 / 3 * PI * (r * r * r);
-	return 0;
+	int a = 10;
+	int b = 8;
+	int c = 7;
+	
 }
--- lab05/src/main.c
+++ lab05/src/main.c
@@ -1,1 +1,11 @@
-
+int main ()
+{
+int num = 31;
+bool l= true;
+for (int i=2; i<num; i++)
+{
+if(num % i == 0)
+{l=false;}
+}
+return 0;
+}
