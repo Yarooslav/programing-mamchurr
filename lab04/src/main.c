0437d64b068a77ae23e9ff19e26335ac006fac51
Yaroslav Mamchur <yaroslavmamchur5@gmail.com>
2021-10-24 09:25:13 +0000

    лаба 4

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -1,8 +1,14 @@
-//задание номер 20
+#include <stdio.h>
+//задача 14
 int main()
 {
-	int a = 10;
-	int b = 8;
-	int c = 7;
-	
+	int  number = 122;
+	int num1 = number / 100 % 10;
+ 	int num2 = number / 10 % 10;
+ 	int num3 = number % 10;
+
+if (num1 == num2 || num2 == num3 || num3 == num1) printf("Одинаковые цифры есть");
+else printf("Одинаковых цифр нет");
+
+return 0;
 }
