7dbbd2546427f91e9a2791c2b2224529886f85f0
yarik21102 <yaroslavmamchur5@gmail.com>
2021-11-04 12:53:12 +0300

    лаба 4

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -2,14 +2,18 @@
 //задача 14
 int main()
 {
-	int  number = 122;
+	int  number = 123;
 	int num1 = number / 100 % 10;
  	int num2 = number / 10 % 10;
  	int num3 = number % 10;
-
-if (num1 == num2 || num2 == num3 || num3 == num1) string a= "Одинаковых цифр есть";
-else string b = "Одинаковых цифр нет";
-
-
+	int res= 0;
+if (num1 == num2 || num2 == num3 || num3 == num1)
+{
+res ++;// если  1 тогда есть одинаковые цифры 
+}
+else 
+{
+res--;// если -1 тогда нету одинаковых цифр 
+}
 return 0;
 }
